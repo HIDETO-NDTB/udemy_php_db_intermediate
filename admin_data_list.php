7e929feb58b984a7f958114b999bced4e266ffee
HIDETO-NDTB <neckwisteria@yahoo.co.jp>
2021-05-20 11:09:12 +0900

    implement update function

--- admin_data_list.php
+++ admin_data_list.php
@@ -46,6 +46,9 @@ $data = $pre->fetchAll(PDO::FETCH_ASSOC);
           <td><a class="btn btn-primary" href="./admin_data_detail.php?test_form_id=<?php echo rawurldecode(
               $d["test_form_id"]
           ); ?>">詳細</a></td>
+          <td><a class="btn btn-primary" href="./admin_data_update.php?test_form_id=<?php echo rawurldecode(
+              $d["test_form_id"]
+          ); ?>">修正</a></td>
         </tr>
     <?php endforeach; ?>
     </table>
